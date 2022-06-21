5d5346e5a39532a7591dd31a6a9414daefe2a74a
girish716 <girishchandra79179@gmail.com>
2022-06-21 11:07:03 +0530

    .env change 4

--- task_manager/server/app.js
+++ task_manager/server/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
       origin: "https://task-manager.girishdama.com/",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
       preflightContinue: false
     })
 )
